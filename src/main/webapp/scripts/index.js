d2c4da94e432faa7e8eefc8b0d3a4fe55879fed0
Yan Chen <yfc210@g.harvard.edu>
2017-10-29 09:22:00 -0400

    added more responsiveness

--- src/main/webapp/scripts/index.js
+++ src/main/webapp/scripts/index.js
@@ -6,4 +6,22 @@ $(function(){
         e.preventDefault();
         $("#wrapper").toggleClass("toggled");
     });
+    
+    $(".editForm input").prop("disabled", true);
+    $(".editForm select").prop("disabled", true);
+    
+    $("#editButton").on("click", function(event){
+    	$(this).closest('.editForm').find('input').prop('disabled', false);
+    	$(this).closest('.editForm').find('select').prop('disabled', false);
+
+    });
+
 });
+
+function goBack(){
+	 window.history.back();
+}
+
+function goFoward(){
+	 window.history.forward();
+}
